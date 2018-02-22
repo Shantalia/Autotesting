4a20824a14295355cef25a60c36cca09b24b65a6
Shantalia <kottya1995@ukr.net>
2018-02-22 11:51:40 +0200

    Added carcass for testing the process of downloading the software.

---
+++ src/test/java/com/ericorporation/autotesting/ericorporation/de/SoftDownload.java
@@ -0,0 +1,20 @@
+package com.ericorporation.autotesting.ericorporation.de;
+
+import com.ericorporation.autotesting.action.SignInAction;
+import com.ericorporation.autotesting.browser.ChromeDriverInstaller;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SoftDownload {
+    private WebDriver webDriver;
+    private SignInAction action;
+
+    @Before
+    public void openWindow() {
+        webDriver = new ChromeDriverInstaller().getDriver();
+        action = new SignInAction(webDriver);
+    }
+}
